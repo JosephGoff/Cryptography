22ec27e50490a33669aa1baf653fa15370b35059
Joseph Goff <40038953+JosephGoff@users.noreply.github.com>
2018-10-16 12:23:09 -0400

    Updated from Brython Server: 10/16/2018 12:23:09 PM

--- cryptography.py
+++ cryptography.py
@@ -22,43 +22,43 @@ while end == "false":
     decryptednumbers = []
     decryptedletters = ""
     userinput = input("Type e to encrypt, d to decrypt, or x to exit: ")
-        if userinput != "e" and userinput != "d" and userinput != "x":
-            print("Invalid command. Please try again. ")
-        elif userinput == "e":
-            userstring = input("Message: ")
-            userkey = input("Key: ")
-            for s in userstring:
-                stringint.append(associations.find(s))
-            for a in userkey:
-                keyint.append(associations.find(a))
-            while len(keyint) <= len(stringint):
-                keyint = keyint + keyint
-            zippednumbers = zip(stringint, keyint)
-            for d in zippednumbers:
-                encryptednumbers.append((d[0] + d[1]))
-            for r in encryptednumbers:
-                if r > 46:
-                    r = r - 46
-                encryptedletters = encryptedletters + associations[r]
-            print(encryptedletters) 
-        elif userinput == "d":
-            usercyphertext = input("Message: ")
-            cyphertextkey = input("Key: ")
-            for y in usercyphertext:
-                cyphertextnumbers.append(associations.find(y))
-            for i in cyphertextkey:
-                cypherkeynumbers.append(associations.find(i))
-            while len(cyphertextnumbers) >= len(cypherkeynumbers):
-                cypherkeynumbers = cypherkeynumbers + cypherkeynumbers
-            zippedcypher = zip(cyphertextnumbers, cypherkeynumbers)
-            for e in zippedcypher:
-                decryptednumbers.append((e[0] - e[1]))
-            for w in decryptednumbers:
-                if w < 0:
-                    w = w + 46
-                decryptedletters = decryptedletters + associations[w]
-            print(decryptedletters)
-        elif userinput == "x":
-            print("You have exited.")
-            end = "true"    
-        
+    if userinput != "e" and userinput != "d" and userinput != "x":
+        print("Invalid command. Please try again. ")
+    elif userinput == "e":
+        userstring = input("Message: ")
+        userkey = input("Key: ")
+        for s in userstring:
+            stringint.append(associations.find(s))
+        for a in userkey:
+            keyint.append(associations.find(a))
+        while len(keyint) <= len(stringint):
+            keyint = keyint + keyint
+        zippednumbers = zip(stringint, keyint)
+        for d in zippednumbers:
+            encryptednumbers.append((d[0] + d[1]))
+        for r in encryptednumbers:
+            if r > 85:
+                r = r - 85
+            encryptedletters = encryptedletters + associations[r]
+        print(encryptedletters) 
+    elif userinput == "d":
+        usercyphertext = input("Message: ")
+        cyphertextkey = input("Key: ")
+        for y in usercyphertext:
+            cyphertextnumbers.append(associations.find(y))
+        for i in cyphertextkey:
+            cypherkeynumbers.append(associations.find(i))
+        while len(cyphertextnumbers) >= len(cypherkeynumbers):
+            cypherkeynumbers = cypherkeynumbers + cypherkeynumbers
+        zippedcypher = zip(cyphertextnumbers, cypherkeynumbers)
+        for e in zippedcypher:
+            decryptednumbers.append((e[0] - e[1]))
+        for w in decryptednumbers:
+            if w < 0:
+                w = w + 85
+            decryptedletters = decryptedletters + associations[w]
+        print(decryptedletters)
+    elif userinput == "x":
+        print("You have exited.")
+        end = "true"    
+    
